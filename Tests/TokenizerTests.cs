09567fce59486be5fe244a6a9e41aeebd973a936
Matt Sherman <mwsherman@gmail.com>
2024-06-06 22:55:01 -0400

    Add streaming API

--- Tests/TokenizerTests.cs
+++ Tests/TokenizerTests.cs
@@ -4,70 +4,137 @@ using uax29;
 using NUnit.Framework.Internal;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualBasic;
 
 [TestFixture]
 public class TestTokenizer
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+	[SetUp]
+	public void Setup()
+	{
+	}
 
-    [Test]
-    public void Reset()
-    {
-        var example = "Hello, how are you?";
-        var bytes = Encoding.UTF8.GetBytes(example);
+	[Test]
+	public void Reset()
+	{
+		var example = "Hello, how are you?";
+		var bytes = Encoding.UTF8.GetBytes(example);
 
-        var tokens = Tokenizer.Create(example);
+		var tokens = Tokenizer.Create(example);
 
-        var first = new List<string>();
-        while (tokens.MoveNext())
-        {
-            // do something with tokens.Current
-            first.Add(tokens.Current.ToString());
-        }
+		var first = new List<string>();
+		while (tokens.MoveNext())
+		{
+			// do something with tokens.Current
+			first.Add(tokens.Current.ToString());
+		}
 
-        Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing
+		Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing
 
-        tokens.Reset();
+		tokens.Reset();
 
-        var second = new List<string>();
-        while (tokens.MoveNext())
-        {
-            // do something with tokens.Current
-            second.Add(tokens.Current.ToString());
-        }
+		var second = new List<string>();
+		while (tokens.MoveNext())
+		{
+			// do something with tokens.Current
+			second.Add(tokens.Current.ToString());
+		}
 
-        Assert.That(first.SequenceEqual(second));
-    }
+		Assert.That(first.SequenceEqual(second));
+	}
 
-    [Test]
-    public void SetText()
-    {
-        var example = "Hello, how are you?";
+	[Test]
+	public void SetText()
+	{
+		var example = "Hello, how are you?";
 
-        var tokens = Tokenizer.Create(example);
+		var tokens = Tokenizer.Create(example);
 
-        var first = new List<string>();
-        while (tokens.MoveNext())
-        {
-            // do something with tokens.Current
-            first.Add(tokens.Current.ToString());
-        }
+		var first = new List<string>();
+		while (tokens.MoveNext())
+		{
+			first.Add(tokens.Current.ToString());
+		}
 
-        Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing
+		Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing
 
-        tokens.SetText(example);
+		tokens.SetText(example);
 
-        var second = new List<string>();
-        while (tokens.MoveNext())
-        {
-            // do something with tokens.Current
-            second.Add(tokens.Current.ToString());
-        }
+		var second = new List<string>();
+		while (tokens.MoveNext())
+		{
+			second.Add(tokens.Current.ToString());
+		}
 
-        Assert.That(first.SequenceEqual(second));
-    }
+		Assert.That(first.SequenceEqual(second));
+	}
+
+	/// <summary>
+	/// Ensure that streamed text and static text return identical results.
+	/// </summary>
+	[Test]
+	public void StreamTest()
+	{
+		var example = "abcdefghijk lmnopq r stu vwxyz; ABC DEFG HIJKL MNOP Q RSTUV WXYZ! 你好，世界.";
+		var examples = new List<string>()
+		{
+			example,											// smaller than the buffer
+			string.Concat(Enumerable.Repeat(example, 999))		// larger than the buffer
+		};
+
+		foreach (var input in examples)
+		{
+			var bytes = Encoding.UTF8.GetBytes(input);
+			var staticTokens = Tokenizer.Create(bytes);
+
+			using var stream = new MemoryStream(bytes);
+			var streamedTokens = Tokenizer.Create(stream);
+
+			while (streamedTokens.MoveNext())
+			{
+				staticTokens.MoveNext();
+
+				var staticCurrent = Encoding.UTF8.GetString(staticTokens.Current);
+				var streamingCurrent = Encoding.UTF8.GetString(streamedTokens.Current);
+
+				Assert.That(staticCurrent, Is.EqualTo(streamingCurrent));
+			}
+
+			Assert.That(staticTokens.MoveNext(), Is.False, "Static tokens should have been consumed");
+			Assert.That(streamedTokens.MoveNext(), Is.False, "Streamed tokens should have been consumed");
+		}
+	}
+
+	[Test]
+	public void SetStream()
+	{
+		var input = "Hello, how are you?";
+		var bytes = Encoding.UTF8.GetBytes(input);
+		using var stream = new MemoryStream(bytes);
+
+		var tokens = Tokenizer.Create(stream);
+
+		var first = new List<string>();
+		while (tokens.MoveNext())
+		{
+			var s = Encoding.UTF8.GetString(tokens.Current);
+			first.Add(s);
+		}
+
+		Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing
+
+		using var stream2 = new MemoryStream(bytes);
+
+		tokens.SetStream(stream2);
+
+		var second = new List<string>();
+		while (tokens.MoveNext())
+		{
+			var s = Encoding.UTF8.GetString(tokens.Current);
+			second.Add(s);
+		}
+
+		Assert.That(first.SequenceEqual(second));
+	}
 }
 
---
+++ uax29/StreamingTokenizer.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+namespace uax29;
+
+/// <summary>
+/// Tokenizer splits a stream of UTF-8 bytes as words, sentences or graphemes, per the Unicode UAX #29 spec.
+/// Use <see cref="StreamingTokenizer{TSpan}.MoveNext"/> to iterate, and <see cref="StreamingTokenizer{TSpan}.Current"/> to retrive the current token (i.e. the word, grapheme or sentence).
+/// </summary>
+public ref struct StreamingTokenizer
+{
+	Tokenizer<byte> tok;
+
+	Buffer buffer;
+
+	/// <summary>
+	/// Tokenizer splits strings (or UTF-8 bytes) as words, sentences or graphemes, per the Unicode UAX #29 spec.
+	/// </summary>
+	/// <param name="stream">A stream of UTF-8 encoded bytes.</param>
+	/// <param name="tokenType">Optional, choose to tokenize words, graphemes or sentences. Default is words.</param>
+	/// <param name="maxTokenBytes">
+	/// Optional, the maximum token size in bytes. Tokens that exceed this size will simply be cut off at this length, no error will occur.
+	/// Defaults is 1024 bytes. The tokenizer is intended for natural language, so we don't expect you'll find text with a token beyond a couple of dozen bytes.
+	/// If this cutoff is too small for your data, increase it. If you'd like to save memory, reduce it.
+	/// </param>
+	internal StreamingTokenizer(Stream stream, TokenType tokenType = TokenType.Words, int maxTokenBytes = 1024)
+	{
+		tok = new Tokenizer<byte>(null, tokenType);
+		buffer = new Buffer(stream, maxTokenBytes);
+	}
+
+	public bool MoveNext()
+	{
+		buffer.Consume(tok.Current.Length); // the previous token
+		var input = buffer.Contents;
+		tok.SetText(input);
+		return tok.MoveNext();
+	}
+
+	public readonly ReadOnlySpan<byte> Current => tok.Current;
+
+	public void SetStream(Stream stream)
+	{
+		this.tok.SetText(null);
+		this.buffer.SetStream(stream);
+	}
+}
+
+internal ref struct Buffer
+{
+	readonly byte[] storage;
+	int end = 0;
+	Stream stream;
+
+	internal Buffer(Stream stream, int size)
+	{
+		this.stream = stream;
+		storage = new byte[size];
+	}
+
+	internal ReadOnlySpan<byte> Contents
+	{
+		get
+		{
+			var read = stream.Read(storage, end, storage.Length - end);
+			end += read;
+
+			return storage.AsSpan(0, end);
+		}
+	}
+
+	internal void Consume(int consumed)
+	{
+		// Move the remaining unconsumed data to the start of the buffer
+		end -= consumed;
+		Array.Copy(storage, consumed, storage, 0, end);
+	}
+
+	internal void SetStream(Stream stream)
+	{
+		this.stream = stream;
+		end = 0;
+	}
+}
