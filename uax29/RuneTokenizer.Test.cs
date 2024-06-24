515e20981ca830159c2e60fbdaf2681f741bbdf0
Matt Sherman <mwsherman@gmail.com>
2024-06-24 17:07:22 -0400

    first rune tokenizer

---
+++ uax29/RuneTokenizer.Test.cs
@@ -0,0 +1,175 @@
+﻿namespace Tests;
+
+using UAX29;
+using System.Linq;
+using System.Text;
+
+[TestFixture]
+public class TestRuneTokenizer
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Basic()
+	{
+		var example = "Hello, how are you, 😀 👨‍❤️‍👩";
+		var runes = new RuneTokenizer<char>(example, Rune.DecodeFromUtf16);
+
+		var got = "";
+		foreach (var rune in runes)
+		{
+			got += rune.ToString();
+		}
+
+		Assert.That(got, Is.EqualTo(example));
+	}
+
+	[Test]
+	public void Reset()
+	{
+		var example = "Hello, how are you?";
+		var bytes = Encoding.UTF8.GetBytes(example);
+
+		var tokens = Tokenizer.GetWords(example);
+
+		var first = new List<string>();
+		foreach (var token in tokens)
+		{
+			first.Add(token.ToString());
+		}
+
+		Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing
+
+		tokens.Reset();
+
+		var second = new List<string>();
+		foreach (var token in tokens)
+		{
+			second.Add(token.ToString());
+		}
+
+		Assert.That(first.SequenceEqual(second));
+	}
+
+	[Test]
+	public void SetText()
+	{
+		var example = "Hello, how are you?";
+
+		var tokens = Tokenizer.GetWords(example);
+
+		var first = new List<string>();
+		foreach (var token in tokens)
+		{
+			first.Add(token.ToString());
+		}
+
+		Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing
+
+		tokens.SetText(example);
+
+		var second = new List<string>();
+		foreach (var token in tokens)
+		{
+			second.Add(token.ToString());
+		}
+
+		Assert.That(first.SequenceEqual(second));
+	}
+
+	[Test]
+	public void Enumerator()
+	{
+		var input = "Hello, how are you?";
+		var mem = input.AsMemory();
+		var bytes = Encoding.UTF8.GetBytes(input);
+		Tokenizer.GetWords(mem);
+
+		var tokens = Tokenizer.GetWords(input);
+		var first = new List<string>();
+		while (tokens.MoveNext())
+		{
+			var s = tokens.Current.ToString();
+			first.Add(s);
+		}
+		Assert.That(first, Has.Count.GreaterThan(1));   // just make sure it did the thing		
+
+		var tokens2 = Tokenizer.GetWords(input);
+		var second = new List<string>();
+		foreach (var token in tokens2)
+		{
+			var s = token.ToString();
+			second.Add(s);
+		}
+		Assert.That(first.SequenceEqual(second));
+	}
+
+	[Test]
+	public void ToList()
+	{
+		var example = "abcdefghijk lmnopq r stu vwxyz; ABC DEFG HIJKL MNOP Q RSTUV WXYZ! 你好，世界.";
+		var tokens = Tokenizer.GetWords(example);
+		var list = tokens.ToList();
+
+		var i = 0;
+		foreach (var token in tokens)
+		{
+			Assert.That(token.SequenceEqual(list[i]));
+			i++;
+		}
+
+		Assert.That(list, Has.Count.EqualTo(i), "ToList should return the same number of tokens as iteration");
+
+		// Tokenizer should reset back to the beginning
+		Assert.That(tokens.start, Is.EqualTo(0));
+		Assert.That(tokens.end, Is.EqualTo(0));
+
+		var threw = false;
+		tokens.MoveNext();
+		try
+		{
+			tokens.ToList();
+		}
+		catch (InvalidOperationException)
+		{
+			threw = true;
+		}
+		Assert.That(threw, Is.True, "Calling ToList after iteration has begun should throw");
+	}
+
+	[Test]
+	public void ToArray()
+	{
+		var example = "abcdefghijk lmnopq r stu vwxyz; ABC DEFG HIJKL MNOP Q RSTUV WXYZ! 你好，世界.";
+		var tokens = Tokenizer.GetWords(example);
+		var array = tokens.ToArray();
+
+		var i = 0;
+		foreach (var token in tokens)
+		{
+			Assert.That(token.SequenceEqual(array[i]));
+			i++;
+		}
+
+		Assert.That(array, Has.Length.EqualTo(i), "ToArray should return the same number of tokens as iteration");
+
+		// Tokenizer should reset back to the beginning
+		Assert.That(tokens.start, Is.EqualTo(0));
+		Assert.That(tokens.end, Is.EqualTo(0));
+
+		var threw = false;
+		tokens.MoveNext();
+		try
+		{
+			tokens.ToArray();
+		}
+		catch (InvalidOperationException)
+		{
+			threw = true;
+		}
+		Assert.That(threw, Is.True, "Calling ToArray after iteration has begun should throw");
+	}
+}
---
+++ uax29/RuneTokenizer.cs
@@ -0,0 +1,124 @@
+using System.Buffers;
+using System.Text;
+
+namespace UAX29;
+
+/// <summary>
+/// Tokenizer splits strings or UTF-8 bytes as words, sentences or graphemes, per the Unicode UAX #29 spec.
+/// </summary>
+/// <typeparam name="T">byte or char, indicating the type of the input, and by implication, the output.</typeparam>
+public ref struct RuneTokenizer<T> where T : struct
+{
+	ReadOnlySpan<T> input;
+
+	readonly Decoder<T> DecodeFirstRune;
+
+	internal int pos = 0;
+	internal Rune rune;
+
+	bool begun = false;
+
+	/// <summary>
+	/// Tokenizer splits strings (or UTF-8 bytes) as words, sentences or graphemes, per the Unicode UAX #29 spec.
+	/// </summary>
+	/// <param name="input">A string, or UTF-8 byte array.</param>
+	/// <param name="tokenType">Choose to split words, graphemes or sentences. Default is words.</param>
+	internal RuneTokenizer(ReadOnlySpan<T> input, Decoder<T> decodeFirstRune)
+	{
+		this.input = input;
+		this.DecodeFirstRune = decodeFirstRune;
+	}
+
+	/// <summary>
+	/// Move to the next token. Use <see cref="Current"/> to retrieve the rune.
+	/// </summary>
+	/// <returns>Whether there are any more runes. False typically means EOF.</returns>
+	public bool MoveNext()
+	{
+		begun = true;
+
+		if (pos >= input.Length)
+		{
+			return false;
+		}
+
+		var status = DecodeFirstRune(input[pos..], out this.rune, out int consumed);
+		if (status != OperationStatus.Done)
+		{
+			// Garbage in, garbage out
+			throw new InvalidOperationException("Rune could not be decoded");
+		}
+
+		pos += consumed;
+		return true;
+	}
+
+	/// <summary>
+	/// The current rune
+	/// </summary>
+	public readonly Rune Current
+	{
+		get
+		{
+			return this.rune;
+		}
+	}
+
+	public readonly RuneTokenizer<T> GetEnumerator()
+	{
+		return this;
+	}
+
+	/// <summary>
+	/// Resets the tokenizer back to the first rune.
+	/// </summary>
+	public void Reset()
+	{
+		this.pos = 0;
+		this.begun = false;
+	}
+
+	/// <summary>
+	/// (Re)sets the text to be tokenized, and resets the iterator back to the the start.
+	/// </summary>
+	public void SetText(ReadOnlySpan<T> input)
+	{
+		Reset();
+		this.input = input;
+	}
+
+	/// <summary>
+	/// Iterates over all runes and collects them into a list, allocating a new array for each token.
+	/// </summary>
+	/// <returns>List<byte[]> or List<char[]>, depending on the input</returns>
+	public List<Rune> ToList()
+	{
+		if (begun)
+		{
+			throw new InvalidOperationException("ToList must not be called after iteration has begun. You may wish to call Reset() on the tokenizer.");
+		}
+
+		var result = new List<Rune>();
+		foreach (var token in this)
+		{
+			result.Add(token);
+		}
+
+		this.Reset();
+		return result;
+	}
+
+	/// <summary>
+	/// Iterates over all runes and collects them into an array, allocating a new array for each token.
+	/// </summary>
+	/// <returns>byte[][] or char[][], depending on the input</returns>
+	public Rune[] ToArray()
+	{
+		if (begun)
+		{
+			throw new InvalidOperationException("ToArray must not be called after iteration has begun. You may wish to call Reset() on the tokenizer.");
+		}
+
+		return this.ToList().ToArray();
+	}
+}
