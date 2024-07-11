dabe2294edb14b5bcf1bf4d87d03a411779092ce
Matt Sherman <mwsherman@gmail.com>
2024-07-11 17:12:16 -0400

    comments

--- uax29/Options.cs
+++ uax29/Options.cs
@@ -13,7 +13,11 @@ public enum Options : byte
 
     /// <summary>
     /// Omit tokens that consist entirely of whitespace, defined as UAX #29 WSegSpace | CR | LF | Tab.
-    ///
+    /// <para>
+    /// “Whitespace” in this implementation includes those which delimit words, but not all characters that are categorically whitespace.
+    /// For example, “non-breaking space” is whitespace, but it’s not what you want when splitting words, and so
+    /// it is not considered whitespace for our purposes.
+    /// </para>
     /// <para>* Only supported for splitting Words; ignored for Graphemes and Sentences. *</para>
     /// </summary>
     OmitWhitespace = 1,
